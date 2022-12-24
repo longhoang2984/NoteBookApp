6229ed8627ec5b8091c1217ef4289d049636f5dc
Long Hoàng <longhoang.2984@gmail.com>
2022-12-25 00:10:39 +0700

    Initial Commit

---
+++ NoteBookApp/NoteBookAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteBookAppApp.swift
+//  NoteBookApp
+//
+//  Created by Cửu Long Hoàng on 25/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct NoteBookAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
